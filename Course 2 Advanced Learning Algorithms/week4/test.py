8095c25e4ab46062e2788ebd9d5fe94c4b5e2f80
SubuMangu <142916612+SubuMangu@users.noreply.github.com>
2024-11-25 15:33:40 -1200

    Add files via upload

---
+++ Course 2 Advanced Learning Algorithms/week4/test.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from utils import build_tree_recursive,generate_tree_viz
+X_train = np.array([[1, 1, 1],
+[0, 0, 1],
+ [0, 1, 0],
+ [1, 0, 1],
+ [1, 1, 1],
+ [1, 1, 0],
+ [0, 0, 0],
+ [1, 1, 0],
+ [0, 1, 0],
+ [0, 1, 0]])
+
+y_train = np.array([1, 1, 0, 0, 1, 1, 0, 1, 0, 0])
+tree = []
+build_tree_recursive(X_train, y_train, [0,1,2,3,4,5,6,7,8,9], "Root", max_depth=1, current_depth=0, tree = tree)
+generate_tree_viz([0,1,2,3,4,5,6,7,8,9], y_train, tree)
